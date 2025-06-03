7a9f60a748ae2644a93524737a1c882110e79a2d
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2025-06-03 17:19:59 -0400

    fix build issue for dtw

--- source/api/api_exports.cpp
+++ source/api/api_exports.cpp
@@ -10,7 +10,9 @@ struct PentaneCStringView {
 	const std::size_t data_len;
 };
 
+#ifdef _M_IX86
 #pragma comment(linker, "/EXPORT:Pentane_LogUTF8=_Pentane_LogUTF8")
+#endif
 extern "C" void Pentane_LogUTF8(PentaneCStringView* c_str) {
 	if (c_str != nullptr) {
 		if (c_str->data == nullptr || c_str->data_len == 0) {
@@ -23,13 +25,17 @@ extern "C" void Pentane_LogUTF8(PentaneCStringView* c_str) {
 	}
 }
 
+#ifdef _M_IX86
 #pragma comment(linker, "/EXPORT:Pentane_IsPluginLoaded=_Pentane_IsPluginLoaded")
+#endif
 extern "C" int Pentane_IsPluginLoaded(PentaneUUID* uuid) {
 	return plugin_loader::is_loaded(uuid) ? 1 : 0;
 }
 
 // FIXME: Is this the best way to do this?
+#ifdef _M_IX86
 #pragma comment(linker, "/EXPORT:Pentane_GetUserLanguage_ISO6391=_Pentane_GetUserLanguage_ISO6391")
+#endif
 extern "C" unsigned short Pentane_GetUserLanguage_ISO6391() {
 	const auto& code = LANGUAGE_CODE[config::language()];
 	return static_cast<unsigned short>(std::bit_cast<unsigned char>(code[0])) << 8 | static_cast<unsigned short>(std::bit_cast<unsigned char>(code[1]));
